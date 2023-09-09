e0c76ddd5c340f44dccb4b10b14c19864aa0dfac
jaypee15 <okoyejohnpaul15@gmail.com>
2023-09-09 19:01:58 +0100

    added format suffix to view

--- apis/urls.py
+++ apis/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from .views import GetInfo
 
 urlpatterns = [
     path("api/", GetInfo.as_view(), name="api"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json'])
